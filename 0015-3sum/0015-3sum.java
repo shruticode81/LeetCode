7621a521ac9808f5f4925b20eb1a64067c95ca3e
shruti anand <vitikaawesome81@gmail.com>
2025-07-23 21:21:28 +0530

    Time: 31 ms (56.79%), Space: 51.1 MB (98.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,59 +1,64 @@
 import java.util.*;
 class Solution {
-    // public List<List<Integer>> threeSum(int[] nums) {
-    //     List<List<Integer>> result = new ArrayList<>();
-    //     int n = nums.length;
-    //     int i=0;
-    //     int j=i+1;
-    //     int k=n-1;
-    //     Arrays.sort(nums);
-
-    //     while(j<k){
-    //         if(nums[i]+nums[j]+nums[k] == 0) {
-    //             List<Integer> tripletPair = new ArrayList<>();
-    //             tripletPair.add(nums[i]);
-    //             tripletPair.add(nums[j]);
-    //             tripletPair.add(nums[k]);
-    //             for(List<Integer> sublist :result){
-    //                 if(sublist.containsAll(tripletPair)){
-    //                     j++;
-    //                     i++;
-    //                     continue;
-    //                 }
-    //             }
-    //             result.add(tripletPair);
-    //             j++;
-    //             i++;
-    //         }else if(nums[i]+nums[j]+nums[k] < 0) {
-    //             j++;
-    //             i++;
-    //         }else{
-    //             k--;
-    //         }
-    //     }
-    //     return result;
-    // }
-
-
-
-     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            HashSet<Integer> seen = new HashSet<>();
-            for(int j=i+1; j<n;j++){
-                int complement = -nums[i]-nums[j];
-                if(seen.contains(complement)){
-                    List<Integer> triplets = Arrays.asList(nums[i],nums[j],complement);
-                    Collections.sort(triplets);
-                    result.add(triplets);
-                }
+        Arrays.sort(nums);
 
-                seen.add(nums[j]);
+        for(int i=0; i<n-2; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
             }
+
+            int j=i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == 0) {
+                    List<Integer> triplets = Arrays.asList(nums[i], nums[j], nums[k]);
+                    result.add(triplets);
+                    while(j<k && nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    j++;
+                    k--;
+                    
+                }else if(sum < 0) {
+                    j++;
+                }else{
+                    k--;
+                }
+             }
+
         }
 
-
-        return new ArrayList<>(result);
+      
+        return result;
     }
+
+
+
+    //  public List<List<Integer>> threeSum(int[] nums) {
+    //     Set<List<Integer>> result = new HashSet<>();
+    //     int n = nums.length;
+    //     for(int i=0;i<n-1;i++){
+    //         HashSet<Integer> seen = new HashSet<>();
+    //         for(int j=i+1; j<n;j++){
+    //             int complement = -nums[i]-nums[j];
+    //             if(seen.contains(complement)){
+    //                 List<Integer> triplets = Arrays.asList(nums[i],nums[j],complement);
+    //                 Collections.sort(triplets);
+    //                 result.add(triplets);
+    //             }
+
+    //             seen.add(nums[j]);
+    //         }
+    //     }
+
+
+    //     return new ArrayList<>(result);
+    // }
 }
