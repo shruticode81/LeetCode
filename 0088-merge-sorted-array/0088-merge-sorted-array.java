edb63289fc085042064c29460a652e56546ce024
shruti anand <vitikaawesome81@gmail.com>
2025-07-25 18:01:37 +0530

    Time: 0 ms (100%), Space: 42.5 MB (15.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,40 +1,40 @@
 import java.util.*;
 class Solution {
-    //  public void merge(int[] nums1, int m, int[] nums2, int n) {
-    //     int i = m - 1, j = n - 1, k = m + n - 1;
-    //     while (j >= 0) {
-    //         if (i >= 0 && nums1[i] > nums2[j]) {
-    //             nums1[k--] = nums1[i--];
-    //         } else {
-    //             nums1[k--] = nums2[j--];
-    //         }
-    //     }
-    // }
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // using shell sort method
-        for(int i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
-        }
-        int len = m+n;
-        int gap = (len/2)+(len%2);
-       
-        while(gap>0){
-            int i=0;
-            int j=gap;
-            while(j<len){
-                if(nums1[i]>nums1[j]){
-                    //swap
-                    int temp = nums1[i];
-                    nums1[i]=nums1[j];
-                    nums1[j]=temp;
-                }
-                i++;
-                j++;
+     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
             }
-            if(gap == 1) break;
-            gap = (gap/2)+(gap%2);
         }
     }
+    // public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //     // using shell sort method
+    //     for(int i=0;i<n;i++){
+    //         nums1[m+i]=nums2[i];
+    //     }
+    //     int len = m+n;
+    //     int gap = (len/2)+(len%2);
+       
+    //     while(gap>0){
+    //         int i=0;
+    //         int j=gap;
+    //         while(j<len){
+    //             if(nums1[i]>nums1[j]){
+    //                 //swap
+    //                 int temp = nums1[i];
+    //                 nums1[i]=nums1[j];
+    //                 nums1[j]=temp;
+    //             }
+    //             i++;
+    //             j++;
+    //         }
+    //         if(gap == 1) break;
+    //         gap = (gap/2)+(gap%2);
+    //     }
+    // }
 
 // below is the code to sort the both the given array (nums1 and num2), then we can merge them
     //    int len = m+n;
