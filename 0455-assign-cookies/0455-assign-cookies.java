81d554ace05f6d46f604d6ed15d7a35e69ef25ea
shruti anand <vitikaawesome81@gmail.com>
2025-08-18 18:01:29 +0530

    Time: 11 ms (35.47%), Space: 48.6 MB (59.23%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        // sort the array
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l=0;
+        int r=0;
+        while(l<g.length && r<s.length){
+            if(g[l]<=s[r]){
+                l++;
+            }
+            r++;
+
+        }
+        return l;
+    }
+}
