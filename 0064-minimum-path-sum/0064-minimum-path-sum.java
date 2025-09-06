53d31a336838dee276752d33d5969dd4a9e7ee01
shruti anand <vitikaawesome81@gmail.com>
2025-09-06 21:32:16 +0530

    Time: 6 ms (10.76%), Space: 47.7 MB (56.23%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -6,25 +6,56 @@ class Solution {
         for(int i=0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
-        return solveMinPathSumM(m-1, n-1, grid, dp);
+        
+        return solveMinPathSumDP(grid, dp);
+
+        // return solveMinPathSumM(m-1, n-1, grid, dp);
 
         // return solveMinPathSum(m-1, n-1, grid);
     }
 
-    int solveMinPathSumM(int i, int j, int[][] grid, int[][] dp){
-        if(i==0 && j==0){
-            return grid[i][j];
+      int solveMinPathSumDP(int[][] grid, int[][] dp){
+        int m = grid.length;
+        int n = grid[0].length;
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0){
+                dp[i][j]=grid[i][j];
+            }else{
+                int up = grid[i][j];
+                if(i>0){
+                    up = up+dp[i-1][j];
+                }else{
+                    up = up+ (int)1e9;
+                }
+                int left = grid[i][j];
+                if(j>0){
+                    left = left+dp[i][j-1];
+                }else{
+                    left = left + (int)1e9;
+                }
+                dp[i][j] = Math.min(up,left);
+            }
         }
-        if(i<0 || j<0){
-            return (int)1e9;
-        }
-        if(dp[i][j]!=-1){
-            return  dp[i][j];
-        }
-        int up = grid[i][j] + solveMinPathSumM(i-1,j,grid,dp);
-        int left = grid[i][j] + solveMinPathSumM(i,j-1,grid,dp);
-         return dp[i][j] = Math.min(up,left);
+       }
+      return dp[m-1][n-1];
+     
     }
+
+    // int solveMinPathSumM(int i, int j, int[][] grid, int[][] dp){
+    //     if(i==0 && j==0){
+    //         return grid[i][j];
+    //     }
+    //     if(i<0 || j<0){
+    //         return (int)1e9;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return  dp[i][j];
+    //     }
+    //     int up = grid[i][j] + solveMinPathSumM(i-1,j,grid,dp);
+    //     int left = grid[i][j] + solveMinPathSumM(i,j-1,grid,dp);
+    //      return dp[i][j] = Math.min(up,left);
+    // }
     
     // int solveMinPathSum(int i, int j, int[][] grid){
     //     if(i==0 && j==0){
