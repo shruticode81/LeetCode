87d9649aecd0d904519ad205faeab3e1678f6911
shruti anand <vitikaawesome81@gmail.com>
2025-09-07 14:17:05 +0530

    Time: 5 ms (33.66%), Space: 45.1 MB (76.43%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -6,25 +6,45 @@ class Solution {
             Arrays.fill(dp[i],-1);
         }
         // return solveMinTotalM(0,0, triangle,dp);
-        return solveMinTotalDP(triangle,dp);
+        // return solveMinTotalDP(triangle,dp);
+        return solveMinTotalDPSpaceOp(triangle);
     }
-    int solveMinTotalDP(List<List<Integer>> triangle, int[][] dp){
+    int solveMinTotalDPSpaceOp(List<List<Integer>> triangle){
             //T.C = o(nxn)
-            // s.c = o(nxn)
+            // s.c = o(n) Space optimization
         int row = triangle.size();
-
+        int prev[] = new int[row];
         for(int j=0;j<row;j++){
-            dp[row-1][j]=triangle.get(row-1).get(j);
+            prev[j]=triangle.get(row-1).get(j);
         }
         for(int i=row-2;i>=0;i--){
+            int temp[] = new int[row];
             for(int j=i;j>=0;j--){
-                int down = triangle.get(i).get(j)+dp[i+1][j];
-                int diagonal = triangle.get(i).get(j)+dp[i+1][j+1];
-                dp[i][j]=Math.min(down, diagonal);
+                int down = triangle.get(i).get(j)+prev[j];
+                int diagonal = triangle.get(i).get(j)+prev[j+1];
+                temp[j] = Math.min(down,diagonal);
             }
+            prev = temp;
         }
-        return dp[0][0];
+        return prev[0];
     }
+    // int solveMinTotalDP(List<List<Integer>> triangle, int[][] dp){
+    //         //T.C = o(nxn)
+    //         // s.c = o(nxn)
+    //     int row = triangle.size();
+
+    //     for(int j=0;j<row;j++){
+    //         dp[row-1][j]=triangle.get(row-1).get(j);
+    //     }
+    //     for(int i=row-2;i>=0;i--){
+    //         for(int j=i;j>=0;j--){
+    //             int down = triangle.get(i).get(j)+dp[i+1][j];
+    //             int diagonal = triangle.get(i).get(j)+dp[i+1][j+1];
+    //             dp[i][j]=Math.min(down, diagonal);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
     // int solveMinTotalM(int i, int j, List<List<Integer>> triangle, int[][] dp){
     //         //T.C = o(nxn)
     //         // s.c = o(nxn)+o(n)
