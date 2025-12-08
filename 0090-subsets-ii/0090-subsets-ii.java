5bc619962dca9ef98baeaa4dd49f6318a6c0020a
shruti anand <vitikaawesome81@gmail.com>
2025-12-08 16:17:15 +0530

    Time: 2 ms (68.93%), Space: 45.2 MB (10.18%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,49 +1,23 @@
-import java.util.*;
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> resultSubsets = new ArrayList<>();
-        List<Integer> subset = new ArrayList<>();
+        
         Arrays.sort(nums);
-        solveUniqueSubsets(0,subset,nums, resultSubsets);
-        return resultSubsets;
-    }
-    void solveUniqueSubsets(int i, List<Integer> subset,int[] nums, List<List<Integer>> resultSubsets){
-        if(nums.length == i){
-            resultSubsets.add(new ArrayList<>(subset));
-            return;
-        }
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+         solveSubsetsWithDup(0, nums,res, ans);
+         return res;
 
-        subset.add(nums[i]);
-        solveUniqueSubsets(i+1, subset, nums, resultSubsets);
-        subset.remove(subset.size()-1);
-        //remove duplicates
-        int ni = i+1;
-        while(ni<nums.length && nums[ni] == nums[i]){
-            ni++;
-        }
-        solveUniqueSubsets(ni, subset, nums, resultSubsets);
+        
     }
 
-    // void solveUniqueSubsets(int i, List<Integer> subset,int[] nums, List<List<Integer>> resultSubsets){
-    //     if(nums.length == i){
-    //         if(!duplicatePresent(resultSubsets, subset)){
-    //             resultSubsets.add(new ArrayList<>(subset));
-    //         }
-    //         return;
-    //     }
-
-    //     subset.add(nums[i]);
-    //     solveUniqueSubsets(i+1, subset, nums, resultSubsets);
-    //     subset.remove(subset.size()-1);
-    //     solveUniqueSubsets(i+1, subset, nums, resultSubsets);
-    // }
-
-    // boolean duplicatePresent(List<List<Integer>> subsets, List<Integer> subset) {
-    //     for(List<Integer> x : subsets){
-    //         if(x.equals(subset)){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+   void solveSubsetsWithDup(int index, int[] nums, List<List<Integer>> res, List<Integer> ans){
+        // base cond
+        res.add(new ArrayList<>(ans));
+        for(int i=index;i<nums.length;i++){
+            if(i>index && nums[i]==nums[i-1]) continue;
+            ans.add(nums[i]);
+            solveSubsetsWithDup(i+1,nums,res,ans);
+            ans.remove(ans.size()-1);
+        }
+    }
 }
