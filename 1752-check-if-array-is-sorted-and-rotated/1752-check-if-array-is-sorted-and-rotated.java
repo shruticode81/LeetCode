696d559638ab342b95a9a94e2693512001d2534d
shruti anand <vitikaawesome81@gmail.com>
2026-02-01 15:50:26 +0530

    Time: 0 ms (100%), Space: 43.3 MB (8.83%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,57 +1,47 @@
 class Solution {
     public boolean check(int[] nums) {
         // base case
+        // if(nums == null || nums.length<=2){
+        //     return true;
+        // }
+        // int drop = 0;
+        // int n = nums.length;
+        // int next = 0;
+        // for(int i=0;i<n;i++){
+        //     if(i == n-1){
+        //         next = nums[0];
+        //     }else{
+        //         next = nums[i+1];
+        //     }
+        //     if(nums[i]>next){
+        //         drop++;
+        //     }
+        //     if(drop>1){
+        //         return false;
+        //     }
+        // }
+        // return true;
         if(nums == null || nums.length<=2){
             return true;
         }
-        int drop = 0;
-        int n = nums.length;
-        int next = 0;
-        for(int i=0;i<n;i++){
-            if(i == n-1){
-                next = nums[0];
-            }else{
-                next = nums[i+1];
-            }
-            if(nums[i]>next){
-                drop++;
-            }
-            if(drop>1){
-                return false;
-            }
-        }
-        return true;
-    //     if(nums == null || nums.length<=2){
-    //         return true;
-    //     }
 
-    //    int drop=0;
-    //    for(int i=1;i<nums.length;i++){
-    //     if(nums[i-1] > nums[i]){
-    //         drop++;
-    //     }
-    //     if(drop>1){
-    //         return false;
-    //     }
-    // }
-    // if(nums[0]<nums[nums.length-1]){
-    //     drop++;
-    // }
+       int drop=0;
+       for(int i=1;i<nums.length;i++){
+        if(nums[i-1] > nums[i]){
+            drop++;
+        }
+    }
+    if(nums[0]<nums[nums.length-1]){
+        drop++;
+    }
     // if(drop>1){
     //     return false;
     // }
 
     // return true;
 
-
+    return drop<=1; 
 
     }
-    // for (int i = 0; i < n; i++) {
-    //         if( i == n-1) next = nums[0];
-    //         else next = nums[i+1];
-    //         //next = nums[(i + 1) % n]; 
-    //         if (nums[i] > next) drops++;
-    //         if (drops > 1) return false;
-    //     }
-    //     return true;
+
 }
